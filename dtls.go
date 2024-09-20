cbed5dc94c2fe9505237b9c483b66756dfa03685
Lakshya Chopra <lakshyachopra2707@gmail.com>
2024-09-20 20:08:03 +0000

    Updated code to add functionality for enc and dec buffers.

--- dtls.go
+++ dtls.go
@@ -17,7 +17,10 @@ import (
 )
 
 type SSLConn struct {
-	ssl *C.SSL
+	ssl         *C.SSL
+	rbio        *C.BIO
+	wbio        *C.BIO
+	encrypt_buf []byte
 }
 
 const (
@@ -27,7 +30,7 @@ const (
 
 func check_error(err_code C.int) {
 	if err_code != 1 {
-		log.Fatalf("Error occurred: %d", C.ERR_get_error())
+		fmt.Errorf("Error occurred: %d", C.ERR_get_error())
 	}
 }
 
@@ -35,20 +38,20 @@ func handle_ssl_error(ssl *C.SSL, err_code C.int) {
 
 	switch err_code {
 	case C.SSL_ERROR_WANT_READ:
-		log.Fatal("SSL ERROR WANT READ")
+		fmt.Errorf("SSL ERROR WANT READ")
 	case C.SSL_ERROR_WANT_WRITE:
-		log.Fatal("SSL ERROR WANT WRITE")
+		fmt.Errorf("SSL ERROR WANT WRITE")
 	case C.SSL_ERROR_ZERO_RETURN:
-		log.Fatal("SSL connection closed\n")
+		fmt.Errorf("SSL connection closed")
 		C.SSL_free(ssl)
 	case C.SSL_ERROR_SYSCALL:
-		log.Fatal("SSL syscall error")
+		fmt.Errorf("SSL syscall error")
 		C.SSL_free(ssl)
 	case C.SSL_ERROR_SSL:
-		log.Fatal("SSL library error\n")
+		fmt.Errorf("SSL library error")
 		C.SSL_free(ssl)
 	default:
-		log.Fatal("Unexpected SSL error\n")
+		fmt.Errorf("Unexpected SSL error")
 		C.SSL_free(ssl)
 	}
 }
@@ -74,13 +77,13 @@ func Init_ssl_ctx(SSLMODE int, certPath string, keyPath string) *C.SSL_CTX {
 		defer C.free(unsafe.Pointer(keyPath))
 
 		if C.SSL_CTX_use_certificate_file(ctx, certPath, C.SSL_FILETYPE_PEM) <= 0 {
-			log.Fatal("Failed to load certificate")
+			fmt.Errorf("Failed to load certificate")
 			C.SSL_CTX_free(ctx)
 			return nil
 		}
 
 		if C.SSL_CTX_use_PrivateKey_file(ctx, keyPath, C.SSL_FILETYPE_PEM) <= 0 {
-			log.Fatal("Failed to load private key")
+			fmt.Errorf("Failed to load private key")
 			C.SSL_CTX_free(ctx)
 			return nil
 		}
@@ -109,7 +112,8 @@ func New_ssl_conn(ctx *C.SSL_CTX, fd int, SSLMODE int) *SSLConn {
 	} else if SSLMODE == SSLMODE_CLIENT {
 		C.SSL_set_connect_state(ssl)
 	} else {
-		log.Fatal("Wrong ssl mode passed. Pass either 0 or 1")
+		fmt.Errorf("Wrong ssl mode passed. Pass either 0 or 1")
+		C.SSL_free(ssl)
 		return nil
 	}
 
@@ -139,6 +143,21 @@ func Do_ssl_handshake(ssl_conn *SSLConn) int {
 	print_ssl_state(ssl_conn.ssl)
 
 	log.Println("SSL handshake done.")
+
+	// set the rbio and wbio
+	rbio := C.BIO_new(C.BIO_s_mem())
+	// we write encrypted data from the socket to rbio using BIO write and then read from using read
+	wbio := C.BIO_new(C.BIO_s_mem())
+
+	if rbio == nil || wbio == nil {
+		fmt.Errorf("SSL BIO error.")
+		return -1
+	}
+
+	C.SSL_set_bio(ssl_conn.ssl, rbio, wbio)
+	ssl_conn.rbio = rbio
+	ssl_conn.wbio = wbio
+
 	return 0
 
 }
@@ -153,12 +172,88 @@ func print_ssl_state(ssl *C.SSL) {
 
 }
 
-// decrypt_buf reads from the SSL connection into the destination buffer.
-func Decrypt_buf(ssl_conn *SSLConn, dest []byte) C.int {
-	// Convert Go byte siice to C pointer.
-	cDest := (unsafe.Pointer(&dest[0]))
-	n := C.SSL_read(ssl_conn.ssl, cDest, C.int(len(dest)))
-	return n
+func read_enc_buf(ssl_conn *SSLConn, src []byte, dest []byte) C.int {
+	src_len := len(src)
+	cSrc := unsafe.Pointer(&src[0])
+	cDest := unsafe.Pointer(&dest[0])
+
+	for src_len > 0 {
+		// Write the encrypted buffer to rbio
+		bytes_written := C.BIO_write(ssl_conn.rbio, cSrc, C.int(src_len))
+		if bytes_written <= 0 {
+			return -1
+		}
+
+		log.Printf("Bytes written to the rbio: %d", bytes_written)
+		src_len -= int(bytes_written)
+
+		// Perform SSL read
+		n := C.SSL_read(ssl_conn.ssl, cDest, C.int(len(dest)))
+		if n <= 0 {
+			return -1
+		}
+
+		// if src_len == 0 {
+		// Convert C pointer (cDest) back to Go slice
+		dec_bytes := C.GoBytes(cDest, n)
+
+		copy(dest, dec_bytes[:n])
+
+		log.Printf("Decrypted bytes copied: %d", n)
+		return n
+		// }
+
+	}
+
+	return 0
+}
+
+func Encrypt_buf(ssl_conn *SSLConn, src []byte, dest []byte) C.int {
+
+	src_len := len(src)
+	cSrc := (unsafe.Pointer(&src[0]))
+	cDest := unsafe.Pointer(&dest[0])
+
+	for src_len > 0 {
+		n := C.SSL_write(ssl_conn.ssl, cSrc, C.int(src_len))
+		log.Printf("Bytes written LENGTH: %d\n", int(n))
+
+		if n <= 0 {
+			fmt.Errorf("failed to encrypt the data\n")
+			return -1
+		}
+		src_len -= int(n)
+		bytes_read := C.BIO_read(ssl_conn.wbio, cDest, C.int(len(dest)))
+		if bytes_read > 0 {
+			log.Printf("Encrypted data length :%d\n", int(bytes_read))
+
+			enc_bytes := C.GoBytes(cDest, bytes_read)
+
+			copy(dest, enc_bytes[:bytes_read])
+
+			return bytes_read
+		}
+
+	}
+	return 0
+
+}
+
+func New_message_decrypt(ssl_conn *SSLConn, src []byte, dest []byte) {
+
+	n := read_enc_buf(ssl_conn, src, dest)
+	if n < 0 {
+		fmt.Errorf("Error decrypting the message")
+	}
+
+}
+func New_message_encrypt(ssl_conn *SSLConn, src []byte, dest []byte) {
+
+	n := Encrypt_buf(ssl_conn, src, dest)
+	if n < 0 {
+		fmt.Errorf("Error encrypting the message")
+	}
+
 }
 
 // func main() {
