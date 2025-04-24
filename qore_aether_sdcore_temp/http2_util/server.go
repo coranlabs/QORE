66b8fb14c73ac6a3f9aa8a0b68445a41c2f84897
lakshya-chopra <lakshya-chopra@users.noreply.github.com>
2025-04-24 22:56:54 +0000

    [AETHER_http2_util] Remove Min version as tlsv13 as the constraint & improve logs

--- qore_aether_sdcore_temp/http2_util/server.go
+++ qore_aether_sdcore_temp/http2_util/server.go
@@ -20,6 +20,23 @@ import (
 	"golang.org/x/net/http2/h2c"
 )
 
+func curveIDToString(id tls.CurveID) string {
+	switch id {
+	case tls.CurveP256:
+		return "P-256"
+	case tls.CurveP384:
+		return "P-384"
+	case tls.CurveP521:
+		return "P-521"
+	case tls.X25519:
+		return "X25519"
+	case tls.X25519Kyber768Draft00:
+		return "X25519-Kyber768-Draft00"
+	default:
+		return fmt.Sprintf("Unknown Curve ID: %d", id)
+	}
+}
+
 // NewServer returns a server instance with HTTP/2.0 and HTTP/2.0 cleartext support
 // If this function cannot open or create the secret log file,
 // **it still returns server instance** but without the secret log and error indication
@@ -46,8 +63,10 @@ func NewServer(bindAddr string, preMasterSecretLogPath string, handler http.Hand
 			KeyLogWriter:              preMasterSecretFile,
 			PQSignatureSchemesEnabled: true,
 			// PreferServerCipherSuites:  true, // deprecated - has no effect
-			MinVersion: tls.VersionTLS13,
+			// MinVersion: tls.VersionTLS13,
 			ClientAuth: tls.NoClientCert,
+			CurvePreferences: []tls.CurveID{
+				tls.X25519Kyber768Draft00, tls.X25519, tls.CurveP256},
 			GetConfigForClient: func(chi *tls.ClientHelloInfo) (*tls.Config, error) {
 
 				fmt.Println(strings.Repeat("-", 30))
@@ -56,14 +75,20 @@ func NewServer(bindAddr string, preMasterSecretLogPath string, handler http.Hand
 
 				fmt.Print("\tSupported Signature Schemes: ")
 				for _, sigScheme := range chi.SignatureSchemes {
-					fmt.Printf("\t\t%v ", sigScheme)
+					fmt.Printf("\t\t%+v ", sigScheme)
 				}
 				fmt.Println()
 
 				// Print the supported curves
 				fmt.Println("\tSupported Curves: ")
 				for _, curve := range chi.SupportedCurves {
-					fmt.Printf("\t\t%v ", curve)
+					fmt.Printf("\t\t%+v ", curve)
+				}
+				fmt.Println()
+
+				fmt.Println("\tCurve Preferences: ")
+				for _, curve := range server.TLSConfig.CurvePreferences {
+					fmt.Printf("\t\t%s\n", curveIDToString(curve))
 				}
 				fmt.Println()
 
